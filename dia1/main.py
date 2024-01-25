582d05b9ce5336c99cabc58a3f329b47b27fb456
helen <hnmurillo24@gmail.com>
2024-01-25 09:24:50 -0500

    trabajo clase

---
+++ dia1/main.py
@@ -0,0 +1,50 @@
+##---------------------------------------------------
+##---- Ejercicio 1 -------
+##----------------------------------------------------
+
+## impresion en la consola
+print("hola gente")
+
+
+
+#_---------------datosprimitivos --
+#1. sitring---
+texto="campus"
+print (texto)
+print (type(texto))
+2#_ int
+numeroEntero=1
+print(numeroEntero)
+3# float
+numeroDecimal=3.1
+print (numeroDecimal)
+#4 Double
+(numerodecimalLargo)=3.141675748788545545541555488
+print (numerodecimalLargo)
+#5 booleen
+booleano=True
+print (booleano)
+#------- Entradas parte delusuario con definicion de tipo de dato primitivo ----
+entradaUsuario=input("ingresa tu nombre:")
+print("tu nombre es", entradaUsuario)
+#-----------Ciclos------------------ 
+#1 ciclo for
+for i  in range (5,10,2):
+    print(i)
+
+#2 ciclo while
+    boleanito=True
+    while boleanito == True #while es lacondicion a cumplir :
+    print ("sigo vivo")
+    boleanito = False
+
+#-------------condicionales-------
+    
+texto1="campus"
+if texto=="campus":
+     print("soy campus")
+else:
+     print("nosoy campus")
+
+
+## Desarollado por Helen Parga-1001301103
